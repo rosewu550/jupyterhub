91878d8d72acd120718a83548972ec154a8c9f8d
alphaxx <ww792115071@gmail.com>
2024-07-12 11:14:32 +0800

    update jupyterhub_config.py

--- backupconfig/jupyterhub_config.py
+++ backupconfig/jupyterhub_config.py
@@ -1840,3 +1840,4 @@ c.Authenticator.allowed_users = {"alphaxx"}
 #  Default: 4
 # c.CryptKeeper.n_threads = 4
 c.LocalAuthenticator.create_system_users = True
+c.DummyAuthenticator.password = "10055pp"
